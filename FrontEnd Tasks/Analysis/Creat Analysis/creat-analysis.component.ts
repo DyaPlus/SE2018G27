8b9143085310b1051881c86e71517cf6b197ba3b
AbdullahHajjaj <36624498+AbdullahHajjaj@users.noreply.github.com>
2019-02-13 22:44:55 -0800

    Add files via upload

---
+++ FrontEnd Tasks/Analysis/Creat Analysis/creat-analysis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creat-analysis',
+  templateUrl: './creat-analysis.component.html',
+  styleUrls: ['./creat-analysis.component.css']
+})
+export class CreatAnalysisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
